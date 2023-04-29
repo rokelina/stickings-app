0a3e237880599f66ae4294ab594d9a26b5edc4bd
Rosina Scampino <rosinascampino@gmail.com>
2023-04-29 18:33:02 -0400

    radio inputs

--- eight-notes-menu.js
+++ eight-notes-menu.js
@@ -5,23 +5,47 @@ const eightNotesMenu = Object.freeze({
   ll: 'LL',
 });
 
-const columns = document.querySelectorAll('#column');
-console.log(columns);
+const menuContainer = document.getElementById('menu-container');
+const columns = document.querySelectorAll('.column');
 
-function createMenu() {
+function onCreateMenu() {
   columns.forEach((column, key) => {
     for (const prop in eightNotesMenu) {
       const child = document.createElement('div');
-      child.classList.add('sticking');
       if (!column.classList.contains('last')) {
-        child.innerHTML = `<input type="checkbox" id="${prop}" key="${key}" name="sticking"/><label for="${prop}" key="${key}">${eightNotesMenu[prop]}</label>`;
+        child.innerHTML = `<input type="radio" id="${prop}${key}" key=${key} name="field${key}" value="${prop}${key}"/>
+        <label for="${prop}${key}" key="${key}">${prop.toUpperCase()}</label>`;
+        child.classList.add('sticking');
         column.appendChild(child);
       } else {
-        child.innerHTML = `<input type="checkbox" id="${prop}" />`;
+        child.innerHTML = `<input type="checkbox" id="${prop}" name="${prop}" value="${prop}"/>`;
+        child.classList.add('select-row');
         column.appendChild(child);
       }
     }
   });
 }
 
-document.addEventListener('DOMContentLoaded', createMenu);
+function selectRow(e) {
+  let isChecked = e.target.checked;
+  const name = e.target.name;
+  const rowCheckboxes = document.getElementsByName(name);
+  for (const checkbox of rowCheckboxes) {
+    checkbox.checked = isChecked;
+  }
+}
+
+function onSelectRow() {
+  const lastColumn = document.querySelector('.last');
+  const lastCheckboxes = lastColumn.querySelectorAll('input[type="checkbox"]');
+  for (const checkbox of lastCheckboxes) {
+    checkbox.addEventListener('change', selectRow);
+  }
+}
+
+function onLoad() {
+  onCreateMenu();
+  onSelectRow();
+}
+
+document.addEventListener('DOMContentLoaded', onLoad);
